46f3ac1cbd4a7554ab536815b1e53ed93dd56afd
elaismarques <58093550+elaismarques@users.noreply.github.com>
2024-10-23 15:41:51 -0300

    Add files via upload

---
+++ atividade11/atividade11/index.js
@@ -0,0 +1,32 @@
+//1
+const Funcionario1 = {
+    Matricula: 12345,
+    Nome: "João Silva",
+    Funcao: "Desenvolvedor"
+};
+
+console.log(Funcionario1);
+
+// //2
+// function Funcionario(matricula, nome, funcao) {
+//     this.Matricula = matricula;
+//     this.Nome = nome;
+//     this.Funcao = funcao;
+// }
+
+// const Funcionario1 = new Funcionario(67890, "Maria Oliveira", "Analista de Sistemas");
+
+// console.log(Funcionario1);
+
+// //3
+// class Funcionario {
+//     constructor(matricula, nome, funcao) {
+//         this.Matricula = matricula;
+//         this.Nome = nome;
+//         this.Funcao = funcao;
+//     }
+// }
+
+// const Funcionario1 = new Funcionario(54321, "Carlos Pereira", "Gerente de Projetos");
+
+// console.log(Funcionario1);
